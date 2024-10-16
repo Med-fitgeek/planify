87a614c4d7f41665ce7d3f90f024c29e8b6b23cc
Medoune <ndongomedoune.ndiaye@gmail.com>
2024-10-17 00:49:47 +0200

    mise a jour des routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
 import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { provideRouter, RouterModule, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import routes from './app.routes';
+import { bootstrapApplication } from '@angular/platform-browser'; // Pour démarrer l'application
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'my-todo-app';
+
 }
+
+  bootstrapApplication(AppComponent, {
+    providers:[
+      provideRouter(routes)
+    ]
+  });
+
